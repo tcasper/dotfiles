9f8fd0909088fbd87c4c447570b2a4d6a509f7fb
tcasper <tmcasper@gmail.com>
2015-07-27 15:25:48 -0400

    Adding a osx defaults template file for ideas

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,1 +1,12 @@
 #Set defaults for osx
+
+sudo -v
+
+#Show/hide hidden files and folders in the finder
+defaults write com.apple.finder AppleShowAllFiles NO
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+
+
